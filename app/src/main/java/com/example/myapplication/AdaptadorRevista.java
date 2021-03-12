560b7468828be501c8cf153aaefedeb99a394641
AuraTaquez <aura.taquez2015@uteq.edu.ec>
2021-03-11 23:38:36 -0500

    nuevo avance

---
+++ app/src/main/java/com/example/myapplication/AdaptadorRevista.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class AdaptadorRevista extends ArrayAdapter<Revista> {
+    public AdaptadorRevista(Context context, List<Revista> datos) {
+        super(context, R.layout.activity_item, datos);
+
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View item = inflater.inflate(R.layout.activity_item, null);
+        TextView nombre = (TextView)item.findViewById(R.id.texnombre);
+        nombre.setText(getItem(position).getName());
+        TextView lblDescripcion = item.findViewById(R.id.description);
+        lblDescripcion.setText(Html.fromHtml(getItem(position).getDescription()));
+        ImageView imageView = (ImageView)item.findViewById(R.id.imgRevista);
+        Glide.with(this.getContext())
+                .load(getItem(position).getPortada())
+                //.error(R.drawable.imgnotfound)
+                .into(imageView);
+        return(item);
+    }
+
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,21 +1,30 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
-import WebService.WebService;
 import WebService.Asynchtask;
+import WebService.WebService;
 
-public  class MainActivity extends AppCompatActivity implements Asynchtask {
+public class MainActivity extends AppCompatActivity implements Asynchtask, AdapterView.OnItemClickListener {
+    ArrayList<Revista> ListaRevista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +33,26 @@ public  class MainActivity extends AppCompatActivity implements Asynchtask {
         Map<String, String> datos = new HashMap<String, String>();
         WebService ws = new WebService("https://revistas.uteq.edu.ec/ws/journals.php", datos, MainActivity.this, MainActivity.this);
         ws.execute("");
+        getPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        getPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+        ListView list = (ListView) findViewById(R.id.ListaHview);
+        list.setOnItemClickListener(this);
     }
-    ArrayList<Revista> ListaRevista;
+
+    public void getPermission(String permission) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!(checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED))
+                ActivityCompat.requestPermissions(this, new String[]{permission}, 1);
+        }
+    }
+
+
     @Override
     public void processFinish(String result) throws JSONException {
         ListaRevista = new ArrayList<>();
-        JSONObject jsonObject = new JSONObject(result);
-        JSONObject jresults = jsonObject.getJSONObject("Results");
-        Iterator<?> iterator = jresults.keys();
-        while (iterator.hasNext()) {
-            String key = (String) iterator.next();
-            JSONObject Revistas = jresults.getJSONObject(key);
+        JSONArray jsonArray = new JSONArray(result);
+        for (int i = 0; i <jsonArray.length(); i++) {
+            JSONObject Revistas = jsonArray.getJSONObject(i);
             Revista revista = new Revista();
             revista.setJournal_id(Revistas.getString("journal_id"));
             revista.setPortada(Revistas.getString(("portada")));
@@ -44,5 +62,15 @@ public  class MainActivity extends AppCompatActivity implements Asynchtask {
             revista.setName(Revistas.getString("name"));
             ListaRevista.add(revista);
         }
+        AdaptadorRevista adapter = new AdaptadorRevista(this, ListaRevista);
+        ListView Hview = findViewById(R.id.ListaHview);
+        Hview.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Intent miIntent = new Intent(MainActivity.this, volumenes.class);
+        miIntent.putExtra("JID", ((Revista) adapterView.getItemAtPosition(position)).getJournal_id());
+        startActivity(miIntent);
     }
 }
